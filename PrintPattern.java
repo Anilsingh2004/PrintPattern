b838d4f8aee2af0cc0a5b08be325ec036e9a174d
Anilsingh2004 <anilsingh869784@gmail.com>
2024-11-27 19:38:52 +0530

    Update PrintPattern.java

--- PrintPattern.java
+++ PrintPattern.java
@@ -1,8 +1,15 @@
 package codingTest;
-
+// print the pattern 
+//1
+//22
+//333
+//4444
+//55555
 public class PrintPattern {
 	public static void printPattern( int n) {
+		        //outer loop
 		   for (int i = 1; i <= n; i++) {
+			   //inner loop
 	            for (int j = 0; j < i; j++) {
 	                System.out.print(i + " ");
 	            }
